5623377bc0463696f527a94f10434ead3d7b1952
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-08 19:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var dodger = document.getElementById('dodger')
-moveDodgerRight() = moveDodgerLeft()
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
