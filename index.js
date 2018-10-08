36950dd0cd1edc1150e9a5db9dac3c20d3ae6ea0
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-10-08 19:42:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function moveDodgerLeft() {
   var left = parseInt(leftNumbers, 10)
  
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left + 1}px`
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+})
